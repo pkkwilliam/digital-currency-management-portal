3e8c2aeb706c24cc7fd1fac4d1f0a22b4f24b9c7
pkkwilliam <pkkwilliam919@gmail.com>
2023-10-15 00:45:46 -0700

    add equity annd PL into ChannelBroker

--- src/pages/ChannelBroker/index.jsx
+++ src/pages/ChannelBroker/index.jsx
@@ -25,6 +25,30 @@ const CHANNEL_ACCOUNT_COLUMNS = [
     key: 'margin',
     render: (text) => <Text type={text > 150 ? 'success' : 'danger'}>{text ?? 0}%</Text>,
   },
+  {
+    title: 'Equity',
+    dataIndex: 'equity',
+    key: 'equity',
+    renderText: (text, record) => `$${text ?? 0}`,
+  },
+  {
+    title: 'Unreleased P/L',
+    dataIndex: 'unreleasedProfitLoss',
+    key: 'unreleasedProfitLoss',
+    tooltip: 'Unreleased Profit/Loss',
+    render: (text) => {
+      const displayValue = text ? text : 0;
+      let type = 'secondary';
+      if (displayValue > 0) {
+        type = 'success';
+      } else if (displayValue < 0) {
+        type = 'danger';
+      } else {
+        type = 'secondary';
+      }
+      return <Text type={type}>{`$${displayValue}`}</Text>;
+    },
+  },
 ];
 
 const ChannelBroker = () => {
